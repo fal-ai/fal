a11af5e857d9325e581b4bafcfe6ad3c7a45a413
Batuhan Taskaya <isidentical@gmail.com>
2023-11-01 20:50:12 +0200

    perf: drop redundant bucket GETs (#936)

--- projects/fal_serverless/src/fal/toolkit/file/providers/gcp.py
+++ projects/fal_serverless/src/fal/toolkit/file/providers/gcp.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import datetime
+import json
 import os
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 from fal.toolkit.file.types import FileData, FileRepository
 from fal.toolkit.mainify import mainify
@@ -12,11 +14,12 @@ DEFAULT_URL_TIMEOUT = 60 * 15  # 15 minutes
 @mainify
 @dataclass
 class GoogleStorageRepository(FileRepository):
-    url_expiration: int | None = field(default_factory=lambda: DEFAULT_URL_TIMEOUT)
-    bucket_name: str | None = field(default="fal_file_storage")
+    bucket_name: str = "fal_file_storage"
+    url_expiration: int | None = DEFAULT_URL_TIMEOUT
     gcp_account_json: str | None = None
 
     _storage_client = None
+    _bucket = None
 
     def __post_init__(self):
         gcp_account_json = self.gcp_account_json
@@ -25,26 +28,29 @@ class GoogleStorageRepository(FileRepository):
             if gcp_account_json is None:
                 raise Exception("GCLOUD_SA_JSON environment secret is not set")
 
-        import json
-
         gcp_account_info = json.loads(gcp_account_json)
 
         from google.cloud.storage import Client
 
         self._storage_client = Client.from_service_account_info(gcp_account_info)
+        self._bucket = self._storage_client.get_bucket(self.bucket_name)
 
-    def save(self, data: FileData) -> str:
-        import datetime
-        from typing import cast
-
-        from google.cloud.storage import Client
-
-        if not isinstance(self._storage_client, Client):
+    @property
+    def storage_client(self):
+        if self._storage_client is None:
             raise Exception("Google Storage client is not initialized")
 
-        client = cast(Client, self._storage_client)
-        bucket = client.get_bucket(self.bucket_name)
-        gcp_blob = bucket.blob(data.file_name)
+        return self._storage_client
+
+    @property
+    def bucket(self):
+        if self._bucket is None:
+            raise Exception("Google Storage bucket is not initialized")
+
+        return self._bucket
+
+    def save(self, data: FileData) -> str:
+        gcp_blob = self.bucket.blob(data.file_name)
         gcp_blob.upload_from_string(data.data, content_type=data.content_type)
 
         if self.url_expiration is None:
