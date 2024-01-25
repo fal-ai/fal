9c9991c487dc2fd21579a2dbd08283da51aa5f6d
badayvedat <badayvedat@gmail.com>
2024-01-25 18:57:59 +0300

    fix: downgrade type checking to py3.9

--- projects/fal/src/fal/toolkit/file/file.py
+++ projects/fal/src/fal/toolkit/file/file.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any, Callable, Optional
 from urllib.parse import urlparse
 
 from fal.toolkit.file.providers.fal import FalFileRepository, InMemoryRepository
@@ -42,15 +42,15 @@ class File(BaseModel):
         description="The URL where the file can be downloaded from.",
         examples=["https://url.to/generated/file/z9RV14K95DvU.png"],
     )
-    content_type: str | None = Field(
+    content_type: Optional[str] = Field(
         description="The mime type of the file.",
         examples=["image/png"],
     )
-    file_name: str | None = Field(
+    file_name: Optional[str] = Field(
         description="The name of the file. It will be auto-generated if not provided.",
         examples=["z9RV14K95DvU.png"],
     )
-    file_size: int | None = Field(
+    file_size: Optional[int] = Field(
         description="The size of the file in bytes.", examples=[4404019]
     )
 
@@ -108,8 +108,8 @@ class File(BaseModel):
     def from_bytes(
         cls,
         data: bytes,
-        content_type: str | None = None,
-        file_name: str | None = None,
+        content_type: Optional[str] = None,
+        file_name: Optional[str] = None,
         repository: FileRepository | RepositoryId = DEFAULT_REPOSITORY,
     ) -> File:
         return cls(
@@ -121,7 +121,7 @@ class File(BaseModel):
     def from_path(
         cls,
         path: str | Path,
-        content_type: str | None = None,
+        content_type: Optional[str] = None,
         repository: FileRepository | RepositoryId = DEFAULT_REPOSITORY,
     ) -> File:
         file_path = Path(path)
