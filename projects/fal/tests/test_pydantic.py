de1b6493b36c35b998904c7a2a0c1fc6e5cad139
Louis Maddox <louismmx@gmail.com>
2024-01-28 18:01:55 +0000

    refactor: split apart Pydantic test and the patch (initially so patch funcs can be reused in `test_apps.py`)

--- projects/fal/tests/test_pydantic.py
+++ projects/fal/tests/test_pydantic.py
@@ -15,109 +15,11 @@ from pydantic._internal._decorators import (
 from pydantic.config import ConfigDict
 from pydantic.fields import FieldInfo
 
-
-def build_pydantic_model(
-    name,
-    model_config: ConfigDict,
-    model_doc: str | None,
-    base_cls,
-    model_module: str,
-    model_fields: dict[str, FieldInfo],
-    model_validators: dict[str, tuple[Callable, ModelValidatorDecoratorInfo]],
-    field_validators: dict[str, tuple[Callable, FieldValidatorDecoratorInfo]],
-    methods: dict,
-):
-    """Recreate the Pydantic model from the deserialised validator info.
-
-    Arguments:
-        name: The name of the model.
-        model_config: The model's configuration settings. (UNUSED)
-        model_doc: The model's docstring.
-        base_cls: The model's base class.
-        model_module: The name of the module the model belongs to.
-        model_fields: The model's fields.
-        model_validators: The model validators of the model.
-        field_validators: The field validators of the model.
-        methods: The methods of the model.
-    """
-    import pydantic
-
-    validators = {
-        **{
-            name: pydantic.model_validator(mode=info.mode)(func)
-            for name, (func, info) in model_validators.items()
-        },
-        **{
-            name: pydantic.field_validator(*info.fields, mode=info.mode)(func)
-            for name, (func, info) in field_validators.items()
-        },
-    }
-    model_fields["__annotations__"] = {
-        name: field.annotation for name, field in model_fields.items()
-    }
-    model_fields.update({name: func for name, (func, _) in field_validators.items()})
-
-    model_cls = pydantic.create_model(
-        name,
-        # __config__=model_config, # UNUSED
-        __doc__=model_doc,
-        __base__=base_cls,
-        __module__=model_module,
-        __validators__=validators,
-        **model_fields,
-    )
-    for method_name, method_funcdef in methods.items():
-        setattr(model_cls, method_name, method_funcdef)
-    return model_cls
-
-
-@dill.register(type(BaseModel))
-def _dill_hook_for_pydantic_models(pickler: dill.Pickler, pydantic_model) -> None:
-    if pydantic_model is BaseModel:
-        dill_serialization.save_type(pickler, pydantic_model)
-        return
-
-    decorators = pydantic_model.__pydantic_decorators__
-    model_validators = {
-        validator_name: (decorator.func, decorator.info)
-        for validator_name, decorator in decorators.model_validators.items()
-    }
-    field_validators = {
-        validator_name: (decorator.func.__func__, decorator.info)
-        for validator_name, decorator in decorators.field_validators.items()
-    }
-
-    methods = {
-        field_name: field_value
-        for field_name, field_value in pydantic_model.__dict__.items()
-        if field_name in ["steps_x2"]
-    }
-    # for class_field_name, class_field_value in pydantic_model.__dict__.items():
-    #     if class_field_name.startswith("_"):
-    #         continue
-    #     elif class_field_name in ("model_fields", "model_config"):
-    #         continue
-    #     elif class_field_name in pydantic_model.model_fields:
-    #         continue
-    #     elif class_field_name in model_validators:
-    #         continue
-    #     elif class_field_name in field_validators:
-    #         continue
-
-    #     class_fields[class_field_name] = class_field_value
-
-    pickled_model = {
-        "name": pydantic_model.__name__,
-        "model_config": pydantic_model.model_config,
-        "model_doc": pydantic_model.__doc__,
-        "base_cls": pydantic_model.__bases__[0],
-        "model_module": pydantic_model.__module__,
-        "model_fields": pydantic_model.model_fields,
-        "model_validators": model_validators,
-        "field_validators": field_validators,
-        "methods": methods,
-    }
-    pickler.save_reduce(build_pydantic_model, tuple(pickled_model.values()))
+from pydantic_patch import (
+    build_pydantic_model,
+    deserialise_pydantic_model,
+    pickler_building_args,
+)
 
 
 class Input(BaseModel):
@@ -138,7 +40,8 @@ class Input(BaseModel):
     def steps_x2(self) -> int:
         """A method which is neither a validator nor provided by Pydantic.
 
-        Computes double of the `num_steps` field value."""
+        Computes double of the `num_steps` field value.
+        """
         return self.num_steps * 2
 
     @field_validator("epochs")
@@ -150,35 +53,16 @@ class Input(BaseModel):
 
     @model_validator(mode="after")
     def increment(self) -> None:
-        """A model post-validator that increments a counter."""
+        """A model post-validator that increments a `validation_counter` attribute."""
         self.validation_counter += 100
 
 
-def deserialise_pydantic_model():
-    """Serialise (`dill.dumps`) then deserialise (`dill.loads`) a Pydantic model.
-
-    The `recurse` setting must be set, counterintuitively, to prevent excessive
-    recursion (refer to e.g. dill issue
-    [#482](https://github.com/uqfoundation/dill/issues/482#issuecomment-1139017499)):
-
-        to limit the amount of recursion that dill is doing to pickle the function, we
-        need to turn on a setting called recurse, but that is because the setting
-        actually recurses over the global dictionary and finds the smallest subset that
-        the function needs to run, which will limit the number of objects that dill
-        needs to include in the pickle.
-    """
-    dill.settings["recurse"] = True
-    serialized_cls = dill.dumps(Input)
-    print("===== DESERIALIZING =====")
-    model_cls = dill.loads(serialized_cls)
-    deserialised_fvs = vars(model_cls)["__pydantic_decorators__"].field_validators
-    pprint(deserialised_fvs)
-    print("===== INSTANTIATING =====")
-    model = model_cls(prompt="a", num_steps=4, epochs=10)
-    return model
-
-
 def validate_deserialisation(model: Input) -> None:
+    """Assert the model instance was deserialised correctly.
+
+    Arguments:
+        model: The model that was produced by dill deserialisation.
+    """
     prompt = model.prompt
     steps = model.num_steps
     steps_x2 = model.steps_x2()
