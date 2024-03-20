d0f9cc044f3fb9b06b68d5b127182eda0541cda1
Ruslan Kuprieiev <kupruser@gmail.com>
2024-03-21 01:14:11 +0200

    fix(auth): always try to open browser but also show url and code
    The prompt is unnecessary, we should just try opening a browser automatically.
    
    Currently the URL tells me to check the code in CLI, which is not shown to me
    if `webbrowser.get()` was able to find a browser. Also then `open_new_tab` is
    not really guaranteed to work, so we are better off just always showing the URL
    as well.

--- projects/fal/src/fal/console/ux.py
+++ projects/fal/src/fal/console/ux.py
@@ -3,15 +3,12 @@ from __future__ import annotations
 import webbrowser
 
 
-def get_browser() -> webbrowser.BaseBrowser | None:
-    """Gets a reference to the default browser, if available.
-
-    This allows us to decide on a flow before showing the actual browser window.
-    It also avoids unwanted output in the console from the standard `webbrowser.open()`.
-
-    See https://stackoverflow.com/a/19199794
-    """
+def maybe_open_browser_tab(url) -> None:
     try:
-        return webbrowser.get()
+        # Avoids unwanted output in the console from the standard `webbrowser.open()`.
+        # See https://stackoverflow.com/a/19199794
+        browser = webbrowser.get()
+
+        browser.open_new_tab(url)
     except webbrowser.Error:
-        return None
+        pass
