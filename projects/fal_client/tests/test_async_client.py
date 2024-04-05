8356943aff0ab160436ddffad1d21b1737e328b8
Batuhan Taskaya <batuhan@python.org>
2024-04-05 10:24:55 -0700

    faat: add basic media uploads to fal-client

--- projects/fal_client/tests/test_async_client.py
+++ projects/fal_client/tests/test_async_client.py
@@ -1,5 +1,8 @@
+import io
+import httpx
 import pytest
 import fal_client
+from PIL import Image
 
 
 @pytest.fixture
@@ -51,3 +54,35 @@ async def test_fal_client_streaming(client: fal_client.AsyncClient):
 
     assert len(events) >= 2
     assert not events[-1]["partial"]
+
+
+async def test_fal_client_upload(
+    client: fal_client.AsyncClient,
+    tmp_path,
+):
+    async with httpx.AsyncClient() as httpx_client:
+        url = await client.upload(b"Hello, world!", content_type="text/plain")
+        response = await httpx_client.get(url)
+        response.raise_for_status()
+
+        assert response.text == "Hello, world!"
+
+        fake_file = tmp_path / "fake.txt"
+        fake_file.write_text("from fake.txt")
+
+        url = await client.upload_file(fake_file)
+        response = await httpx_client.get(url)
+        response.raise_for_status()
+
+        assert response.text == "from fake.txt"
+
+        image = Image.new("RGB", (100, 100))
+
+        url = await client.upload_image(image)
+        response = await httpx_client.get(url)
+        response.raise_for_status()
+
+        response_image = Image.open(io.BytesIO(response.content))
+        assert response_image.size == (100, 100)
+        assert response_image.mode == "RGB"
+        assert response_image.getpixel((0, 0)) == (0, 0, 0)
