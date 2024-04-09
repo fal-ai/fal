8908b2e3e9150c6998628d9bb945091dda4bbe26
Ruslan Kuprieiev <kupruser@gmail.com>
2024-04-09 04:48:51 +0300

    chore: try using pytest-xdist and XS machines to speedup tests (#168)
    * chore: try using pytest-xdist to speedup tests
    
    * chore: use XS machines for testing instead of M

--- projects/fal/src/fal/_serialization.py
+++ projects/fal/src/fal/_serialization.py
@@ -164,6 +164,25 @@ def patch_exceptions():
 
 
 @mainify
+def _patch_console_thread_locals() -> None:
+    # NOTE: we __sometimes__ might have to serialize these
+    from rich.console import ConsoleThreadLocals
+
+    @dill.register(ConsoleThreadLocals)
+    def save_console_thread_locals(pickler, obj):
+        args = {
+            "theme_stack": obj.theme_stack,
+            "buffer": obj.buffer,
+            "buffer_index": obj.buffer_index,
+        }
+
+        def unpickle(kwargs):
+            return ConsoleThreadLocals(**kwargs)
+
+        pickler.save_reduce(unpickle, (args,), obj=obj)
+
+
+@mainify
 def patch_dill():
     import dill
 
@@ -172,6 +191,7 @@ def patch_dill():
     patch_exceptions()
     patch_pydantic_class_attributes()
     patch_pydantic_field_serialization()
+    _patch_console_thread_locals()
 
 
 @mainify
