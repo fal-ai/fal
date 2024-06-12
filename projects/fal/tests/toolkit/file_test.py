6078cd169d4ddd54238d5b530bb5ff7aa3948dab
Ruslan Kuprieiev <kupruser@gmail.com>
2024-06-12 03:13:13 +0300

    better error

--- projects/fal/tests/toolkit/file_test.py
+++ projects/fal/tests/toolkit/file_test.py
@@ -90,7 +90,9 @@ def test_uniqueness_of_file_name(repo: RepositoryId | FileRepository):
 
     host_and_path = file.url.split("?")[0]
     last_path = host_and_path.split("/")[-1]
-    assert last_path.endswith(".py"), "The file name should end with the same extension"
+    assert (
+        last_path.endswith(".py")
+    ), f"The file name {last_path} should end with the same extension"
     assert (
         len(last_path) > 10
-    ), "There should be a long enough random string in the file name"
+    ), f"There should be a long enough random string in the file name {last_path}"
