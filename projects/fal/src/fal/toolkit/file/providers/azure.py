6c585e9903473ccb740db43cad2519c4c4dea3af
mbukeRepo <mbuke404@gmail.com>
2024-06-14 15:59:19 +0200

    fix: clean up

--- projects/fal/src/fal/toolkit/file/providers/azure.py
+++ projects/fal/src/fal/toolkit/file/providers/azure.py
@@ -22,13 +22,17 @@ class AzureBlobStorageRepository(FileRepository):
         if account_key is None:
             account_key = os.environ.get("AZURE_STORAGE_ACCOUNT_KEY")
             if account_key is None:
-                raise Exception("AZURE_STORAGE_ACCOUNT_KEY environment secret is not set")
+                raise Exception(
+                    "AZURE_STORAGE_ACCOUNT_KEY environment secret is not set"
+                )
 
 
         account_url = f"https://{self.account_name}.blob.core.windows.net"
         default_credential = DefaultAzureCredential()
         self.blob_service = BlobServiceClient(account_url, default_credential)
-        self.container_client = self.blob_service.get_container_client(self.containter_name)
+        self.container_client = self.blob_service.get_container_client(
+            self.containter_name
+        )
         self._storage_client = self.container_client
     @property
     def storage_client(self):
@@ -40,7 +44,10 @@ class AzureBlobStorageRepository(FileRepository):
     def save(self, data: FileData) -> str:
         destination_path = os.path.join(self.folder, data.file_name)
         container_client = self.blob_service.get_container_client(self.containter_name)
-        blob_client = container_client.get_blob_client(self.containter_name, data.file_name)
+        blob_client = container_client.get_blob_client(
+            self.containter_name,
+            data.file_name
+        )
 
         with open(file=destination_path, mode="rb") as file_data:
             blob_client.upload_blob(file_data)
