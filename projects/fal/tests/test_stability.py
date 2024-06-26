7fcd3cc19c359e3f5abae5a1d45cf5e27979d449
Ruslan Kuprieiev <kupruser@gmail.com>
2024-06-27 02:52:46 +0300

    fix(test): chore: use v2.fal.media instead of fal-cdn.fly.dev
    Depends on https://github.com/fal-ai/isolate-cloud/pull/1976

--- projects/fal/tests/test_stability.py
+++ projects/fal/tests/test_stability.py
@@ -560,7 +560,7 @@ def test_worker_env_vars(isolated_client):
         "repo_type, url_prefix",
         [
             ("fal", "https://storage.googleapis.com/isolate-dev-smiling-shark_toolkit_bucket/"),
-            ("fal_v2", "https://fal-cdn.fly.dev/files"),
+            ("fal_v2", "https://v2.fal.media/files"),
         ]
 )
 def test_fal_storage(isolated_client, repo_type, url_prefix):
