bd55f9bd3120f390aaddbe6a70db50766dcf622f
badayvedat <badayvedat@gmail.com>
2024-08-03 20:35:01 +0300

    feat: add app exception handling

--- projects/fal/src/fal/exceptions/__init__.py
+++ projects/fal/src/fal/exceptions/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import annotations
 
-from ._base import FalServerlessException  # noqa: F401
+from ._base import AppException, FalServerlessException, FieldException  # noqa: F401
+from ._cuda import CUDAOutOfMemoryException, is_cuda_oom_exception  # noqa: F401
