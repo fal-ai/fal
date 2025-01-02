2eb335ab878a4b2576043cb00ae416b75ff1e187
squat <lserven@gmail.com>
2025-01-02 11:02:48 +0100

    misc(container): add tests for container apps and remove build_env
    We currently do not exercise container apps in our tests, the container
    app seems to be an unused fixture. This commit introduces basic tests to
    ensure that container apps are built and work as expected. Furthermore,
    this commit introduces a check that container args actually work.
    Finally, this commit removes the `build_env` keyword from the
    `ContainerImage` class. Docker has no concept of specifying environment
    variables at build time except by declaring build arguments. Having that
    keyword is messy and opens the door to non-hermetic builds, which is
    a recipe for leaks and reproducibility problems.
    
    Signed-off-by: squat <lserven@gmail.com>

--- projects/fal/src/fal/container.py
+++ projects/fal/src/fal/container.py
@@ -3,7 +3,7 @@ class ContainerImage:
     from a Dockerfile.
     """
 
-    _known_keys = {"dockerfile_str", "build_env", "build_args", "registries"}
+    _known_keys = {"dockerfile_str", "build_args", "registries"}
 
     @classmethod
     def from_dockerfile_str(cls, text: str, **kwargs):
