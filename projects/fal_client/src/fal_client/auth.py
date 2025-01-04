fb6e0fcfacbdf09aaa71908f6515174394c2f536
Lain <hhichri60@gmail.com>
2025-01-04 03:14:39 +0100

    fix(auth): reorder credential fetching logic to prioritize Google Colab token

--- projects/fal_client/src/fal_client/auth.py
+++ projects/fal_client/src/fal_client/auth.py
@@ -54,14 +54,14 @@ FAL_RUN_HOST = os.environ.get("FAL_RUN_HOST", "fal.run")
 
 
 def fetch_credentials() -> str:
-    if colab_token := get_colab_token():
-        return colab_token
     if key := os.getenv("FAL_KEY"):
         return key
     elif (key_id := os.getenv("FAL_KEY_ID")) and (
         fal_key_secret := os.getenv("FAL_KEY_SECRET")
     ):
         return f"{key_id}:{fal_key_secret}"
+    elif colab_token := get_colab_token():
+        return colab_token
     else:
         raise MissingCredentialsError(
             "Please set the FAL_KEY environment variable to your API key, or set the FAL_KEY_ID and FAL_KEY_SECRET environment variables."
