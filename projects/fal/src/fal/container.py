d4f3ff2c50cd406a4c1caa97f0825ee12931a3dd
Ruslan Kuprieiev <kupruser@gmail.com>
2025-05-08 23:10:37 +0300

    feat(container): don't set builder by default (#500)
    feat(container): don't set bulder by default

--- projects/fal/src/fal/container.py
+++ projects/fal/src/fal/container.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass, field
-from typing import Dict, Literal
+from typing import Dict, Literal, Optional
 
 Builder = Literal["depot", "service", "worker"]
 BUILDERS = {"depot", "service", "worker"}
-DEFAULT_BUILDER: Builder = "depot"
 DEFAULT_COMPRESSION: str = "gzip"
 DEFAULT_FORCE_COMPRESSION: bool = False
 
@@ -17,7 +16,7 @@ class ContainerImage:
     dockerfile_str: str
     build_args: Dict[str, str] = field(default_factory=dict)
     registries: Dict[str, Dict[str, str]] = field(default_factory=dict)
-    builder: Builder = field(default=DEFAULT_BUILDER)
+    builder: Optional[Builder] = field(default=None)
     compression: str = DEFAULT_COMPRESSION
     force_compression: bool = DEFAULT_FORCE_COMPRESSION
 
@@ -30,7 +29,7 @@ class ContainerImage:
                         "Username and password are required for each registry"
                     )
 
-        if self.builder not in BUILDERS:
+        if self.builder and self.builder not in BUILDERS:
             raise ValueError(
                 f"Invalid builder: {self.builder}, must be one of {BUILDERS}"
             )
