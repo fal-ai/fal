03933a099a7162fad68463ab906aea6982d2a822
Matteo Ferrando <matteo.ferrando2@gmail.com>
2025-09-16 09:44:42 -0400

    chore: stop adding usr.id tag to logs
    We stopped sending to Datadog a long time ago (https://github.com/fal-ai/fal/pull/95) and this just slows down all operations

--- projects/fal/src/fal/logging/__init__.py
+++ projects/fal/src/fal/logging/__init__.py
@@ -6,7 +6,6 @@ import structlog
 from structlog.typing import EventDict, WrappedLogger
 
 from .style import LEVEL_STYLES
-from .user import AddUserIdProcessor
 
 # Unfortunately structlog console processor does not support
 # more general theming as a public API. Consider a PR on the
@@ -43,7 +42,6 @@ structlog.configure(
         structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
         structlog.processors.StackInfoRenderer(),
-        AddUserIdProcessor(),
         _console_log_output,
     ],
     wrapper_class=structlog.stdlib.BoundLogger,
