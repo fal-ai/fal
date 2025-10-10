814f56e24a810d066b2a066e7c6d0748ed0ab7a0
Warlord-K <warlordk28@gmail.com>
2025-10-10 22:52:22 +0530

    Don't change directory when app_files is not provided

--- projects/fal/src/fal/app.py
+++ projects/fal/src/fal/app.py
@@ -421,17 +421,16 @@ class App(BaseServable):
             cls.host_kwargs["max_multiplexing"] = cls.max_multiplexing
 
         if cls.kind is not None:
-            cls.host_kwargs["kind"] = cls.kind
-            if cls.kind == "container" and cls.app_files is not []:
-                raise ValueError(
-                    "app_files is not supported for container apps."
-                )
-
+            if cls.kind == "container" and cls.app_files:
+                raise ValueError("app_files is not supported for container apps.")
 
         if cls.image is not None:
             cls.host_kwargs["image"] = cls.image
 
         cls.app_name = getattr(cls, "app_name") or app_name
+
+        if kwargs.get("kind") and cls.app_files:
+            raise ValueError("app_files is not supported for container apps.")
 
         if cls.__init__ is not App.__init__:
             raise ValueError(
@@ -464,7 +463,8 @@ class App(BaseServable):
 
     @asynccontextmanager
     async def lifespan(self, app: fastapi.FastAPI):
-        _include_app_files_path(self.local_file_path, self.app_files_context_dir)
+        if self.app_files:
+            _include_app_files_path(self.local_file_path, self.app_files_context_dir)
         _print_python_packages()
         await _call_any_fn(self.setup)
         try:
