96349267fd4ee6b90fbb9105a5f6c82f0f66fcfa
hanchchch <cndghks15@gmail.com>
2025-12-10 13:26:21 +0900

    fix: optional healthcheck fields

--- projects/fal/src/fal/sdk.py
+++ projects/fal/src/fal/sdk.py
@@ -544,23 +544,48 @@ class MachineRequirements:
             raise ValueError("No machine type provided.")
 
 
-@dataclass
 class HealthCheck:
-    start_period_seconds: int = 0
-    timeout_seconds: int = 5
-    failure_threshold: int = 3
-    call_regularly: bool = True
-    allow_on_busy: bool = True
+    def __init__(
+        self,
+        *,
+        start_period_seconds: Optional[int] = None,
+        timeout_seconds: Optional[int] = None,
+        failure_threshold: Optional[int] = None,
+        call_regularly: Optional[bool] = None,
+        allow_on_busy: Optional[bool] = None,
+    ):
+        """Health check configuration for a runner.
+
+        Args:
+            start_period_seconds: Minimum time the runner has been running \
+            before considering the runner unhealthy when health check fails. \
+            Defaults to startup_timeout of the application, or 30s if not set.
+            timeout_seconds: Timeout in seconds for the health check \
+            request. Defaults to 5 seconds.
+            failure_threshold: Number of consecutive failures \
+            before considering the runner as unhealthy. Defaults to 3.
+            call_regularly: Perform health check every 15s. \
+            If false, only do it when the x-fal-runner-health-check header is present. \
+            Defaults to True.
+            allow_on_busy: Do not perform health check \
+            if the runner has inflight requests. Defaults to False.
+        """
+
+        self.start_period_seconds = start_period_seconds
+        self.timeout_seconds = timeout_seconds
+        self.failure_threshold = failure_threshold
+        self.call_regularly = call_regularly
+        self.allow_on_busy = allow_on_busy
 
 
 @dataclass
 class ApplicationHealthCheckConfig:
     path: str
-    start_period_seconds: int
-    timeout_seconds: int
-    failure_threshold: int
-    call_regularly: bool
-    allow_on_busy: bool
+    start_period_seconds: Optional[int]
+    timeout_seconds: Optional[int]
+    failure_threshold: Optional[int]
+    call_regularly: Optional[bool]
+    allow_on_busy: Optional[bool]
 
 
 @dataclass
