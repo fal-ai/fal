dca0e95aca41ba317637b541233873478452c572
hanchchch <cndghks15@gmail.com>
2026-01-05 16:00:27 +0900

    fix: nested if

--- projects/fal/src/fal/toolkit/file/providers/fal.py
+++ projects/fal/src/fal/toolkit/file/providers/fal.py
@@ -1278,10 +1278,9 @@ class FalFileRepositoryV3(FileRepository):
         }
 
         # Inject the CDN token if it is available
-        if (current_app := get_current_app()) and (
-            cdn_token := current_app.request_headers.get("x-fal-cdn-token")
-        ):
-            headers["X-Fal-CDN-Token"] = cdn_token
+        if current_app := get_current_app():
+            if cdn_token := current_app.request_headers.get("x-fal-cdn-token"):
+                headers["X-Fal-CDN-Token"] = cdn_token
 
         return headers
 
