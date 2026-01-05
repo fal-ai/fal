05f729aab992b0402471a58fcbb71d08ebee002b
hanchchch <cndghks15@gmail.com>
2026-01-05 16:11:40 +0900

    fix: assert if current_app already set

--- projects/fal/src/fal/ref.py
+++ projects/fal/src/fal/ref.py
@@ -5,13 +5,14 @@ if TYPE_CHECKING:
 
 # Global reference to the currently running app
 # This is useful for accessing the app in runtime
-current_app: "App" = None  # type: ignore[assignment]
+current_app: "App" | None = None  # type: ignore[assignment]
 
 
 def set_current_app(app: "App"):
     global current_app  # noqa: PLW0603
+    assert current_app is None, "current_app is already set"
     current_app = app
 
 
-def get_current_app() -> "App":
+def get_current_app() -> "App" | None:
     return current_app
