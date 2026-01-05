5d740e7e8791c0de7e3db5af29b19d61a46ca1c7
hanchchch <cndghks15@gmail.com>
2026-01-05 16:18:19 +0900

    fix: app | none

--- projects/fal/src/fal/ref.py
+++ projects/fal/src/fal/ref.py
@@ -1,11 +1,11 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional
 
 if TYPE_CHECKING:
     from fal.app import App
 
 # Global reference to the currently running app
 # This is useful for accessing the app in runtime
-current_app: "App" | None = None  # type: ignore[assignment]
+current_app: Optional["App"] = None  # type: ignore[assignment]
 
 
 def set_current_app(app: "App"):
