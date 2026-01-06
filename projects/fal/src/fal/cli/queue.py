1a954e536bf84e13c8cc829c55ab0b72fe4f3c4e
hanchchch <cndghks15@gmail.com>
2026-01-06 09:57:50 +0900

    feat: queue flush cli supports caller_user_id

--- projects/fal/src/fal/cli/queue.py
+++ projects/fal/src/fal/cli/queue.py
@@ -43,12 +43,15 @@ def _queue_flush(args):
 
     client = SyncServerlessClient(host=args.host, team=args.team)._create_rest_client()
     user = _get_user(client)
+    caller_user_id = args.caller_user_id
 
     url = f"{client.base_url}/applications/{user.username}/{args.app_name}/queue"
     headers = client.get_headers()
 
     with httpx.Client(base_url=client.base_url, headers=headers, timeout=300) as c:
-        resp = c.delete(url)
+        resp = c.delete(
+            url, params={"caller_user_id": caller_user_id} if caller_user_id else None
+        )
 
     if resp.status_code != HTTPStatus.OK:
         try:
@@ -99,5 +102,9 @@ def add_parser(main_subparsers, parents):
     flush_parser.add_argument(
         "app_name",
         help="Application name.",
+    )
+    flush_parser.add_argument(
+        "caller_user_id",
+        help="Caller user ID.",
     )
     flush_parser.set_defaults(func=_queue_flush)
