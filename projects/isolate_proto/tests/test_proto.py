db6d185d934e932ab8da6857e3dd455210f2cf08
badayvedat <badayvedat@gmail.com>
2026-09-17 14:43:46 -0700

    feat(isolate-proto): add data volume protobuf definitions

--- projects/isolate_proto/tests/test_proto.py
+++ projects/isolate_proto/tests/test_proto.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import isolate_proto
+from google.protobuf import descriptor_pb2, descriptor_pool, message_factory
 
 
 def test_proto():
@@ -140,6 +141,63 @@ def test_callable_entrypoint_fields():
     )
     assert register_request_last_write_wins.WhichOneof("callable") == "entrypoint"
     assert register_request_last_write_wins.entrypoint == "pkg.mod:App.run"
+
+
+def test_volume_fields_are_protobuf_compatible_with_old_readers():
+    file_descriptor = descriptor_pb2.FileDescriptorProto(
+        name="legacy_volume_compat.proto",
+        package="legacy_volume_compat",
+        syntax="proto3",
+    )
+    for message_name, known_field_name, known_field_number in (
+        ("HostedRun", "environment_name", 6),
+        ("RegisterApplicationRequest", "application_name", 5),
+    ):
+        message = file_descriptor.message_type.add(name=message_name)
+        message.field.add(
+            name=known_field_name,
+            number=known_field_number,
+            label=descriptor_pb2.FieldDescriptorProto.LABEL_OPTIONAL,
+            type=descriptor_pb2.FieldDescriptorProto.TYPE_STRING,
+        )
+
+    pool = descriptor_pool.DescriptorPool()
+    pool.Add(file_descriptor)
+    legacy_run_type = message_factory.GetMessageClass(
+        pool.FindMessageTypeByName("legacy_volume_compat.HostedRun")
+    )
+    legacy_registration_type = message_factory.GetMessageClass(
+        pool.FindMessageTypeByName(
+            "legacy_volume_compat.RegisterApplicationRequest"
+        )
+    )
+
+    run = isolate_proto.HostedRun(
+        environment_name="main",
+        volumes=[isolate_proto.VolumeMount(volume_name="data", mount_path="/data")],
+    )
+    registration = isolate_proto.RegisterApplicationRequest(
+        application_name="legacy",
+        volumes=[isolate_proto.VolumeMount(volume_name="data", mount_path="/data")],
+    )
+
+    legacy_run = legacy_run_type.FromString(run.SerializeToString())
+    legacy_registration = legacy_registration_type.FromString(
+        registration.SerializeToString()
+    )
+
+    assert legacy_run.environment_name == "main"
+    assert legacy_registration.application_name == "legacy"
+
+    current_run = isolate_proto.HostedRun.FromString(
+        legacy_run_type(environment_name="main").SerializeToString()
+    )
+    current_registration = isolate_proto.RegisterApplicationRequest.FromString(
+        legacy_registration_type(application_name="legacy").SerializeToString()
+    )
+
+    assert list(current_run.volumes) == []
+    assert list(current_registration.volumes) == []
 
 
 def test_build_environment_field_presence():
